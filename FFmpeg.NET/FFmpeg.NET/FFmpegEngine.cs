13d98680091c9d41e8978ee3338d3e2e3cf68862
Matthew White <skyphio@gmail.com>
2015-09-11 22:42:01 -0400

    Initial commit

---
+++ FFmpeg.NET/FFmpeg.NET/FFmpegEngine.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Threading;
+
+namespace FFmpeg.NET
+{
+    public class FFmpegEngine : IDisposable
+    {
+        public string FFmpegDirectoryPath
+        {
+            get { return Path.GetDirectoryName(FFmpegFilePath); }
+        }
+
+        public string FFmpegFilePath { get; private set; }
+
+        public Mutex FFmpegMutex { get; private set; }
+
+        public Process FFmpegProcess { get; private set; }
+
+        public FFmpegEngine()
+        {
+            FFmpegFilePath = Path.Combine(Path.GetTempPath(), "FFmpeg.NET\\ffmpeg.exe");
+            FFmpegMutex = new Mutex(false, "FFmpeg.NET");
+
+            if (!Directory.Exists(FFmpegDirectoryPath))
+                Directory.CreateDirectory(FFmpegDirectoryPath);
+
+            if (!File.Exists(FFmpegFilePath))
+                DecompressExecutable();
+
+            if (Document.IsLocked(FFmpegFilePath))
+                try
+                {
+                    FFmpegMutex.WaitOne();
+                    KillProcesses();
+                }
+                finally
+                {
+                    FFmpegMutex.ReleaseMutex();
+                }
+        }
+
+        public void Convert(string inputPath, string outputPath, string parameters)
+        {
+            if (string.IsNullOrWhiteSpace(inputPath))
+                throw new ArgumentNullException("inputPath");
+
+            if (!File.Exists(inputPath))
+                throw new FileNotFoundException("The input file could not be found!");
+
+            if (string.IsNullOrWhiteSpace(outputPath))
+                throw new ArgumentNullException("outputPath");
+
+            try
+            {
+                FFmpegMutex.WaitOne();
+                var processStartInfo = new ProcessStartInfo
+                {
+                    Arguments = string.Format("-i \"{0}\" {1} \"{2}\"", inputPath, parameters, outputPath),
+                    CreateNoWindow = true,
+                    FileName = FFmpegFilePath,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = false,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                };
+                using (FFmpegProcess = Process.Start(processStartInfo))
+                    FFmpegProcess.WaitForExit();
+            }
+            finally
+            {
+                FFmpegMutex.ReleaseMutex();
+            }
+        }
+
+        public void Dispose()
+        {
+            FFmpegMutex.Dispose();
+            FFmpegProcess.Dispose();
+            Directory.Delete(FFmpegDirectoryPath, true);
+        }
+
+        private void DecompressExecutable()
+        {
+            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FFmpeg.NET.Resources.ffmpeg.exe.gz"))
+            {
+                using (var compressedFileStream = new FileStream(FFmpegFilePath + ".gz", FileMode.Create))
+                {
+                    resourceStream.CopyTo(compressedFileStream);
+                }
+            }
+
+            var compressedFileInfo = new FileInfo(FFmpegFilePath + ".gz");
+
+            using (FileStream originalFileStream = compressedFileInfo.OpenRead())
+            {
+                string currentFileName = compressedFileInfo.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - compressedFileInfo.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+        }
+
+        public static void Decompress(FileInfo fileToDecompress)
+        {
+
+        }
+
+        private void KillProcesses()
+        {
+            var processes = Process.GetProcessesByName("ffmepg.exe");
+            if (processes.Length <= 0)
+                return;
+
+            foreach (var process in processes)
+                process.Kill();
+        }
+    }
+
+    internal static class Document
+    {
+        internal static bool IsLocked(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new ArgumentNullException("filePath");
+
+            var fileInfo = new FileInfo(filePath);
+            var fileStream = Stream.Null;
+
+            try
+            {
+                fileStream = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                if (fileStream != Stream.Null)
+                    fileStream.Dispose();
+            }
+            return false;
+        }
+    }
+}
---
+++ FFmpeg.NET/FFmpeg.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FFmpeg.NET")]
+[assembly: AssemblyDescription("This is a simple pseudo-wrapper for FFmpeg. It runs FFmpeg hidden, in the background feeding it command-line arguments.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyphio Solutions")]
+[assembly: AssemblyProduct("FFmpeg.NET")]
+[assembly: AssemblyCopyright("Copyright © Matthew White 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3c102eff-9afa-4522-bab5-04eb1346eb37")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
